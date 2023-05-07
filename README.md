406b92b95a82a7ee748d1b1e1d012b908ed345ac
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-05-06 23:35:43 -0700

    Restructured HTML and implemented menu button.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # portfolio
-# Started on 12 MAR 2023: created basic HTML, javaScript and CSS files.  
+Live preview: <https://mikeycos.github.io/portfolio/index.html>
+---
+06 MAY 2023: Restructured HTML, and restarted CSS.  
+12 MAR 2023: created basic HTML, javaScript and CSS files.  
--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log(`Hello world!`);
+let menuButton = document.querySelector('.menu');
+
+menuButton.addEventListener('click', (e) => {
+    let navbar = document.getElementById('navbar');
+    let navItems = navbar.querySelector('.container');
+    // navItems.style.cssText = 'display: block';
+    if(navItems.style.display === 'flex') {
+        navItems.style.display = 'none';
+    } else {
+        navItems.style.display = 'flex';
+    }
+});
